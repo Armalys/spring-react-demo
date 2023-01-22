6dfb2cdd3360c67067452a5977553f64dac8652c
Armalys <mashnert@mail.ru>
2023-01-22 21:25:18 +0400

    minor change

--- src/main/java/com/thehecklers/aircraftpositions/AircraftPositionsApplication.java
+++ src/main/java/com/thehecklers/aircraftpositions/AircraftPositionsApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AircraftPositionsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(AircraftPositionsApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(AircraftPositionsApplication.class, args);
+    }
 }
